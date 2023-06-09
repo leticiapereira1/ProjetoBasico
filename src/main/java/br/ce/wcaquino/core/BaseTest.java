c56f12851c6b28d53529533f811246873d96790e
Leticia dos Santos Pereira <leticia.pereira@mv.com.br>
2023-06-09 16:39:44 -0300

    Initial commit

---
+++ src/main/java/br/ce/wcaquino/core/BaseTest.java
@@ -0,0 +1,42 @@
+package br.ce.wcaquino.core;
+
+import static br.ce.wcaquino.core.DriverFactory.getDriver;
+import static br.ce.wcaquino.core.DriverFactory.killDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+import br.ce.wcaquino.pages.LoginPage;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class BaseTest extends LoginPage {
+	@Rule
+	public TestName testName = new TestName();
+
+	@Before
+	public void inicializa(){
+		acessarTelaInicial();
+		setEmail("ss@gmail");
+		setSenha("123456");
+		entrar();
+	}
+	
+	@After
+	public void finaliza() throws IOException{
+		TakesScreenshot ss = (TakesScreenshot) getDriver();
+		File arquivo = ss.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(arquivo, new File("target" + File.separator + "screenshot" +
+				File.separator + testName.getMethodName() + ".jpg"));
+		
+		if(Propriedades.FECHAR_BROWSER) {
+			killDriver();
+		}
+	}
+
+}
